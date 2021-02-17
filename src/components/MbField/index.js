d524598e74608ca2e49c04f9067d4911f380e695
Syutkina Dasha <syutkina.dasha@gmail.com>
2021-02-17 16:30:27 +0400

    add random

--- src/components/MbField/index.js
+++ src/components/MbField/index.js
@@ -7,12 +7,12 @@ const mbRows = [1,2,3,4,5,6,7,8,9,10];
 const mbCols = ['a','b','c','d','e','f','g','h','i','j'];
 
 function mdFieldElements(ships) {
-	return mbRows.reduce((acc, rVal) => {
-		return [...acc, ...mbCols.map((cVal) => {
+	return mbRows.reduce((acc, rVal, rKey) => {
+		return [...acc, ...mbCols.map((cVal, cKey) => {
 			const itemId = `${cVal}${rVal}`;
 			return {
 				id: itemId,
-				ship: false,
+				ship: ships[cKey][rKey] === 2,
 				open: false,
 			}
 		})]
@@ -20,7 +20,6 @@ function mdFieldElements(ships) {
 }
 
 const MbField = () => {
-	const randomShips = getRandomShips();
 	const [elements, setElements] = useState([]);
 	const [col, setCol] = useState('');
 	const [row, setRow] = useState('');
@@ -45,7 +44,7 @@ const MbField = () => {
 	}, [col, row, elements])
 
 	useEffect(() => {
-		setElements(mdFieldElements(randomShips));
+		setElements(mdFieldElements(getRandomShips()));
 	}, [])
 
 	return (
@@ -55,16 +54,24 @@ const MbField = () => {
 				<label htmlFor="mbr">Row</label><input id="mbr" type="text" maxLength="2" size="1" onChange={(e) => onChange('row', e)} value={row}/>
 				<button type="button" onClick={onSubmit}>Отправить</button>
 			</div>
-			<div className="MbField__list">
-				{
-					elements.map((item) => {
-						return (
-							<div
-								key={item.id}
-								className={cn('MbField__item', {'MbField__item_open': item.open === true}, {'MbField__item_ship': item.ship === true})} />
-						)
-					})
-				}
+			<div className="MbField__list-wrap">
+				<div className="MbField__cols">
+					{ mbCols.map((col, key) => (<div key={`col_${key}`} className="MbField__cols-item">{col}</div>)) }
+				</div>
+				<div className="MbField__rows">
+					{ mbRows.map((row, key) => (<div key={`row_${key}`} className="MbField__rows-item">{row}</div>)) }
+				</div>
+				<div className="MbField__list">
+					{
+						elements.map((item) => {
+							return (
+								<div
+									key={item.id}
+									className={cn('MbField__item', {'MbField__item_open': item.open === true}, {'MbField__item_ship': item.ship === true && item.open === true})} />
+							)
+						})
+					}
+				</div>
 			</div>
 		</div>
 	)
--- src/utils/getRandomShips.js
+++ src/utils/getRandomShips.js
@@ -1,5 +1,6 @@
+const arrLength = 10;
+
 export default function getRandomShips() {
-	const arrLength = 10;
 	const ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
 	let matrix = [];
 
@@ -11,30 +12,107 @@ export default function getRandomShips() {
 	}
 
 	ships.forEach((ship) => {
-		matrix = createShip(ship, matrix);
+		createShip(ship, matrix);
 	})
+
+	return matrix;
 }
 
 function createShip(ship, matrix) {
-	let x0 = Math.round(- 0.5 + Math.random() * (10));
-	let y0 = Math.round(- 0.5 + Math.random() * (10));
+	const orientation = ship !== 1 ? getOrientation() : 0;
+	const [y0, x0] = getRandomCoords();
+	let startX = x0;
+	let startY = y0;
+	const resultMatrix = [...matrix];
 
-	// if (matrix[x0][y0] && matrix[x0][y0] !== 0) {
-	// 	let flag = false;
-	// 	while(!flag){
-	// 		x0 = Math.round(- 0.5 + Math.random() * (10));
-	// 		y0 = Math.round(- 0.5 + Math.random() * (10));
-	//
-	// 		if (matrix[x0][y0] === 0)
-	// 			flag = true;
-	// 	}
-	// }
-	//
-	// if (9 - x0 >= ship) {
-	// 	for (let i=0; i<ship; i++){
-	// 		matrix[x0+i][y0] = 2;
-	// 	}
-	// }
+	if (resultMatrix[y0][x0] !== 0) {
+		return createShip(ship, resultMatrix)
+	} else {
+		const coords = [];
+
+		for (let length = 0; length < ship; length++) {
+			if (startX < arrLength && startY < arrLength && resultMatrix[startY][startX] === 0 && !coords.some(([y,x]) => y === startY && x === startX)){
+				coords.push([startY, startX]);
+
+				if (orientation === 0)
+					startY += 1;
+				else
+					startX += 1;
+			} else {
+				if (orientation === 0)
+					startY = startY === arrLength ? y0 - 1 : startY - 1;
+				else
+					startX = startX === arrLength ? x0 - 1 : startX - 1;
+
+				if (startX < arrLength && startY < arrLength && resultMatrix[startY][startX] === 0 && !coords.some(([y,x]) => y === startY && x === startX)){
+					coords.push([startY, startX]);
+				} else {
+					break;
+				}
+			}
+		}
+
+		if (coords.length === ship) {
+			coords.forEach(([y, x]) => {
+				resultMatrix[y][x] = 2;
+			})
+			setCellsAround(resultMatrix, coords, orientation);
+		} else
+			return createShip(ship, resultMatrix);
+	}
+
+	return resultMatrix;
+}
+
+function getRandomCoords() {
+	const x = Math.abs(Math.round(- 0.5 + Math.random() * (10)));
+	const y = Math.abs(Math.round(- 0.5 + Math.random() * (10)));
+
+	return [x, y];
+}
+
+function getOrientation() {
+	const arr = [0, 1];
+	return arr[Math.floor((Math.random()*arr.length))];
+}
+
+function setCellsAround(matrix, coords, orientation) {
+	const prevArr = [];
+	const nextArr = [];
+	let main = 0;
+	let min = 0;
+	let max = 0;
+
+	if (orientation === 0) {
+		min = coords[0][0];
+		main = coords[0][1];
+		coords.forEach(([y,x]) => {
+			min = y < min ? y : min;
+			max = y > max ? y : max;
+			prevArr.push([y, x-1]);
+			nextArr.push([y, x+1]);
+		})
+	} else {
+		min = coords[0][1];
+		main = coords[0][0];
+		coords.forEach(([y,x]) => {
+			min = x < min ? x : min;
+			max = x > max ? x : max;
+			prevArr.push([y-1, x]);
+			nextArr.push([y+1, x]);
+		})
+	}
+
+	const cells = [...prevArr, ...nextArr];
+	if (orientation === 0)
+		cells.push([min-1, main-1], [min-1, main], [min-1, main+1], [max+1, main-1], [max+1, main], [max+1, main+1]);
+	else
+		cells.push([main-1, min-1], [main, min-1], [main+1, min-1], [main-1, max+1], [main, max+1], [main+1, max+1]);
+
+	cells.forEach(([y,x]) => {
+		if (y >= 0 && x >= 0 && y !== arrLength && x !== arrLength)
+			matrix[y][x] = 1;
+	})
 
 	return matrix;
 }
